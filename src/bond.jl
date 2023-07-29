70cd9f05c16beff2067bb3a3a37a184f6def07cb
yoshiatsu163 <atsushi.yoshida.github@gmail.com>
2023-07-29 19:16:23 +0900

    fix bug in placing bond around pbc (WIP)

--- src/bond.jl
+++ src/bond.jl
@@ -145,56 +145,50 @@ function bond_pbc(s::AbstractSystem, color_func::Function)
         # ボンドが周期境界をまたぐときα, β, γのいずれかが1となる
         # ↓progress: t, cross_dim: 周期強化に交差する次元(1,2,3)，交差しなければ0
         cross_points = NamedTuple{(:progress, :cross_dim), Tuple{Float64, Int64}}[]
-        println(diff)
-        if diff == [0, 0, 0]
-            # 周期境界との交差無し，bondの始点と終点のみ
-            cross_points = [
-                (progress=0.0, cross_dim=0),
-                (progress=1.0, cross_dim=0)
-            ]
-        else
-            if diff[1] != 0
-                # x軸方向の周期境界とbondが交差
-                α = diff[1]
-                (_t, β, γ) = (-p1 + α*a + origin) \ [bond_vector -b -c]
-                push!(cross_points, (progress=_t, cross_dim=1))
-            end
-            if diff[2] != 0
-                β = diff[2]
-                (_t, α, γ) = (-p1 + β*b + origin) \ [bond_vector -a -c]
-                push!(cross_points, (progress=_t, cross_dim=2))
-            end
-            if diff[3] != 0
-                γ = diff[3]
-                (_t, α, β) = (-p1 + γ*c + origin) \ [bond_vector -a -b]
-                push!(cross_points, (progress=_t, cross_dim=3))
-            end
+        resize!(cross_points, count(!=(0), diff)+3)
+        #bondの始点，中点，終点
+        cross_points[1] = (progress=0.0, cross_dim=0)
+        cross_points[end-1] = (progress=0.5, cross_dim=0)
+        cross_points[end] = (progress=1.0, cross_dim=0)
+        n = 2
+        if diff[1] != 0
+            # x軸方向の周期境界とbondが交差
+            α = diff[1]
+            (_t, β, γ) = (-p1 + α*a + origin) \ [bond_vector -b -c] # wrong
+            cross_points[n] = (progress=_t, cross_dim=1)
+            n += 1
         end
+        if diff[2] != 0
+            β = diff[2]
+            (_t, α, γ) = (-p1 + β*b + origin) \ [bond_vector -a -c]
+            cross_points = (progress=_t, cross_dim=2)
+            n += 1
+        end
+        if diff[3] != 0
+            γ = diff[3]
+            (_t, α, β) = (-p1 + γ*c + origin) \ [bond_vector -a -b]
+            cross_points = (progress=_t, cross_dim=3)
+            n += 1
+        end
+        sort!(cross_points; by=x->x.progress)
         @assert all(x -> -1 <= x.progress <= 1, cross_points)
 
-        # add center point of bond
-        sort!(cross_points; by=x->x.progress)
-        center = (progress=0.5, cross_dim=0)
-        ic = searchsortedfirst(cross_points, center; by=x->x.progress)
-        insert!(cross_points, ic, center)
-
-        #if length(cross_points) > 2
-        #    println(cross_points)
-        #end
+        println(cross_points)
 
         # 結合次数別にbonds::Vector{BondData}を構築し，bond dataを追加
         color1, color2 = color_func(s, atom_id1), color_func(s, atom_id2)
         bond_type = _bond_type(s, atom_id1, atom_id2)
         pbc_shift = zeros(3)
-        for i in 2:length(cross_points)
-            t, t_prev = cross_points[i].progress, cross_points[i-1].progress
+        for i in 1:length(cross_points)-1
+            t, t_next = cross_points[i].progress, cross_points[i+1].progress
             cl = t < 0.5 ? color1 : color2
-            p  = p1 + t*bond_vector + pbc_shift
+            p  = p1 + t_next*bond_vector + pbc_shift
+            println(p)
             add_bdata!(
                 bonds[bond_type],
-                p, -(t-t_prev)*bond_vector, cl
+                p, (t-t_next)*bond_vector, cl
             )
-            dim = cross_points[i].cross_dim
+            dim = cross_points[i+1].cross_dim
             if dim != 0
                 pbc_shift -= diff[dim] * axis[:, dim]
             end
